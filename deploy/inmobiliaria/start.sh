9f07e052ec31d376ae22032a49cc3fd5ba679fe9
Christian Bohm <christian.bohm@etana.com>
2026-08-08 19:40:52 -0300

    more

--- deploy/inmobiliaria/start.sh
+++ deploy/inmobiliaria/start.sh
@@ -4,7 +4,9 @@ set -a
 . ./.env
 set +a
 
-mkdir -p ${APPS_WORKSPACE}/inmobiliaria
+mkdir -p "${APPS_WORKSPACE}/inmobiliaria"
+mkdir -p "${APPS_WORKSPACE}/gateway"
+rsync -a --delete ./etc/gateway/ "${APPS_WORKSPACE}/gateway/"
 
 echo "Starting core components ..."
 
