3a5b5d02cf2a905372908f4c01e7928280b46dc4
Christian Bohm <christian.bohm@etana.com>
2026-08-10 20:32:28 -0300

    more

--- registry/start.sh
+++ registry/start.sh
@@ -4,9 +4,10 @@ set -a
 . ./.env
 set +a
 
-mkdir -p "${APPS_WORKSPACE}/inmobiliaria"
+
+mkdir -p ~/dev-cbohm/registry-data
 echo "Starting core components ..."
 
-docker compose -p byb up -d inmobiliaria
+docker compose -p infra up -d registry
 
 echo "Start up complete."
