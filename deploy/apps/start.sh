04b028210ec7a55dfc5fbaed7bff5bfba8963dd3
Christian Bohm <christian.bohm@etana.com>
2026-08-17 19:08:22 -0300

    more

--- deploy/apps/start.sh
+++ deploy/apps/start.sh
@@ -5,8 +5,10 @@ set -a
 set +a
 
 mkdir -p "${APPS_WORKSPACE}/inmobiliaria"
+mkdir -p "${APPS_WORKSPACE}/avisos-catolicos"
 echo "Starting core components ..."
 
 docker compose -p byb up -d inmobiliaria
+docker compose -p byb up -d avisos-catolicos
 
 echo "Start up complete."
